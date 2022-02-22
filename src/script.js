a51ce577326375b0ab210bdb9b54a83cce8e3f79
Gabriel Silva <gabriel_santos2006@hotmail.com>
2022-02-22 19:42:14 -0300

    finished adjusts

--- src/script.js
+++ src/script.js
@@ -4,17 +4,24 @@ const menuButton = document.querySelector(".menu_button");
 menuButton.addEventListener("click", showMenu)
 
 function showMenu(){
-    const menuDescr = document.querySelector(".header_container_menu")
+
+    const menuDescr = document.querySelector(".header_container_menu");
+    const buscar = document.getElementById("divBusca");
+    const carrinhoCompras = document.getElementById("carrinho");
     let displayStatus = menuDescr.style.display;
     if( displayStatus === "flex"){
         menuDescr.style.display = "none";
-        menuButton.style.backgroundColor = "#FFFFFF"
+        menuButton.style.backgroundColor = "#FFFFFF";
+        buscar.style.display = "block";
+        carrinhoCompras.style.display = "block";
         
     
     }
     if(displayStatus !== "flex"){
         menuDescr.style.display = "flex";   
-        menuButton.style.backgroundColor = "#2E2E2E"
+        menuButton.style.backgroundColor = "#2E2E2E";
+        buscar.style.display = "none";
+        carrinhoCompras.style.display = "none";
    
 
         
